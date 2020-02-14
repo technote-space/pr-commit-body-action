115da59ceecb3296cc303151758ab407ef309a8a
Technote <technote.space@gmail.com>
2020-02-14 13:37:03 +0900

    fix: only merged PR

--- src/utils/pulls.ts
+++ src/utils/pulls.ts
@@ -6,7 +6,8 @@ import { PullsInfo } from '../types';
 export const getMergedPulls = async(octokit: Octokit, context: Context): Promise<Array<PullsInfo>> => (await octokit.paginate(octokit.pulls.list.endpoint.merge({
 	...context.repo,
 	base: Utils.getPrBranch(context),
-}))).map((item: Octokit.PullsListResponseItem): PullsInfo => ({
+	state: 'closed',
+}))).filter(item => !!item.merged_at).map((item: Octokit.PullsListResponseItem): PullsInfo => ({
 	author: item.user.login,
 	title: item.title,
 	number: item.number,
