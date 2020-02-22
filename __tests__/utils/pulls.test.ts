fc7ea6279e229353293a51baa881dad43fa9c51a
Technote <technote.space@gmail.com>
2020-02-22 23:55:53 +0900

    feat: filter PR (#23)

--- __tests__/utils/pulls.test.ts
+++ __tests__/utils/pulls.test.ts
@@ -2,6 +2,7 @@
 import nock from 'nock';
 import { resolve } from 'path';
 import {
+	testEnv,
 	generateContext,
 	disableNetConnect,
 	getApiFixture,
@@ -9,6 +10,7 @@ import {
 } from '@technote-space/github-action-test-helper';
 import { getMergedPulls } from '../../src/utils/pulls';
 
+const rootDir        = resolve(__dirname, '../..');
 const fixtureRootDir = resolve(__dirname, '..', 'fixtures');
 const octokit        = getOctokit();
 const context        = generateContext({owner: 'hello', repo: 'world', ref: 'refs/pull/123/merge'}, {
@@ -22,6 +24,7 @@ const context        = generateContext({owner: 'hello', repo: 'world', ref: 'ref
 });
 
 describe('getMergedPulls', () => {
+	testEnv(rootDir);
 	disableNetConnect(nock);
 
 	it('should get merged pull request', async() => {
@@ -32,6 +35,18 @@ describe('getMergedPulls', () => {
 
 		expect(await getMergedPulls(octokit, context)).toEqual([
 			{author: 'octocat', number: 1347, title: 'Amazing new feature (#456)'},
+			{author: 'octocat', number: 1348, title: 'feat: add new feature1 (#123, #234)'},
+		]);
+	});
+
+	it('should get filtered merged pull request', async() => {
+		process.env.INPUT_FILTER_PR = 'true';
+		nock('https://api.github.com')
+			.persist()
+			.get('/repos/hello/world/pulls?base=feature%2Fchange&state=closed')
+			.reply(200, () => getApiFixture(fixtureRootDir, 'pulls.list'));
+
+		expect(await getMergedPulls(octokit, context)).toEqual([
 			{author: 'octocat', number: 1348, title: 'feat: add new feature1 (#123, #234)'},
 		]);
 	});
