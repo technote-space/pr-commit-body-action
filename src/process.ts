7ab2eafc105e410ac85db664b511f3b4bab5c1c6
Technote <technote.space@gmail.com>
2020-09-22 16:35:07 +0900

    feat: use log helper

--- src/process.ts
+++ src/process.ts
@@ -1,6 +1,7 @@
 import {Context} from '@actions/github/lib/context';
 import {Octokit} from '@technote-space/github-action-helper/dist/types';
-import {Logger, Utils} from '@technote-space/github-action-helper';
+import {Utils} from '@technote-space/github-action-helper';
+import {Logger} from '@technote-space/github-action-log-helper';
 import {getCommitItems} from './utils/commit';
 import {
   getBodyTemplate,
